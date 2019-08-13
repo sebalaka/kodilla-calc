c31a1abff7d195a1d80e466cd441a7ad13860d7b
sebalaka <sebalaka@gmail.com>
2019-08-13 20:28:51 +0200

    First Commit

---
+++ src/main/java/com/calcurator2/kodillacalc/Calculator.java
@@ -0,0 +1,4 @@
+package com.calcurator2.kodillacalc;
+
+public class Calculator {
+}
---
+++ src/main/java/com/calcurator2/kodillacalc/KodillaCalcApplication.java
@@ -0,0 +1,13 @@
+package com.calcurator2.kodillacalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCalcApplication.class, args);
+    }
+
+}
