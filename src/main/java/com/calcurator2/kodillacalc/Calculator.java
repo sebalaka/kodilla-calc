22a0c8662291f8fce0a2e176e54f865d082850d9
sebalaka <sebalaka@gmail.com>
2019-08-13 20:45:54 +0200

    Second Commit

--- src/main/java/com/calcurator2/kodillacalc/Calculator.java
+++ src/main/java/com/calcurator2/kodillacalc/Calculator.java
@@ -1,4 +1,12 @@
 package com.calcurator2.kodillacalc;
 
 public class Calculator {
+
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int sub(int num1, int num2){
+        return num1 - num2;
+    }
 }
--- src/main/java/com/calcurator2/kodillacalc/KodillaCalcApplication.java
+++ src/main/java/com/calcurator2/kodillacalc/KodillaCalcApplication.java
@@ -8,6 +8,10 @@ public class KodillaCalcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaCalcApplication.class, args);
-    }
 
+        Calculator Calc = new Calculator();
+        System.out.println(Calc.add(2,4));
+        System.out.println(Calc.sub(2,4));
+    }
 }
+
